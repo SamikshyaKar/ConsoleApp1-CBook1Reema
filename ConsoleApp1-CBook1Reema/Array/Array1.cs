e367ecb279e1d73125cb251632ee1d900d359921
Samikshya <write2samikshyakar@gmail.com>
2021-10-30 13:23:54 +0530

    Add project files.

---
+++ ConsoleApp1-CBook1Reema/Array/Array1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1_CBook1Reema.Array
+{
+    public class Array1
+    {
+        public static Random rand = new Random();
+
+        public static void FindSecLargest()
+        {
+            int[] arr = new int[10];
+            for(int i=0;i<arr.Length;i++)
+            {
+                arr[i] = rand.Next(1, 100);
+            }
+            foreach(int i in arr)
+            {
+                Console.Write( "  " + i);
+            }
+            int largest = arr[0];int lar_pos = 0;
+
+            for(int i=0; i<arr.Length;i++)
+            {
+                if(arr[i] > largest)
+                {
+                    largest = arr[i];
+                    lar_pos = i;
+                }
+
+            }
+            int Sec_largest = arr[arr.Length-lar_pos-1]; int Sec_largePos = 0;
+            for(int i=0;i<arr.Length; i++)
+            {
+                if(i!=lar_pos)
+                {
+                    if (arr[i] > Sec_largest)
+                    {
+                        Sec_largest = arr[i];
+                        Sec_largePos = i;
+                    }
+                       
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(" The Largest Position is " + lar_pos + " And the Largest Number is " + largest);
+            Console.WriteLine();
+            Console.WriteLine(" The Second Largest Position is " + Sec_largePos + " And The Second Largest Num " +Sec_largest);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1-CBook1Reema/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ConsoleApp1_CBook1Reema.Array;
+
+
+namespace ConsoleApp1_CBook1Reema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Array1.FindSecLargest();
+        }
+    }
+}
